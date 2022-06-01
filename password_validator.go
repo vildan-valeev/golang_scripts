a0f635796b801e840b6dd7553878d3b32e151c0e
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-06-02 01:54:42 +0500

    stepik 1.5.8

---
+++ password_validator.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+	"unicode/utf8"
+)
+
+// validator проверяет строку на соответствие некоторому условию
+// и возвращает результат проверки
+type validator func(s string) bool
+
+func IsAny(s string, test func(ch rune) bool) bool {
+	for _, char := range s {
+		if test(char) {
+			return true
+		}
+	}
+	return false
+}
+
+// digits возвращает true, если s содержит хотя бы одну цифру
+// согласно unicode.IsDigit(), иначе false
+func digits(s string) bool {
+	return IsAny(s, unicode.IsDigit)
+}
+
+// letters возвращает true, если s содержит хотя бы одну букву
+// согласно unicode.IsLetter(), иначе false
+func letters(s string) bool {
+	return IsAny(s, unicode.IsLetter)
+}
+
+// minlen возвращает валидатор, который проверяет, что длина
+// строки согласно utf8.RuneCountInString() - не меньше указанной
+func minlen(length int) validator {
+	return func(s string) bool {
+		return utf8.RuneCountInString(s) >= length
+	}
+}
+
+// and возвращает валидатор, который проверяет, что все
+// переданные ему валидаторы вернули true
+func and(funcs ...validator) validator {
+	return func(s string) bool {
+		for _, val := range funcs {
+			if !val(s) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// or возвращает валидатор, который проверяет, что хотя бы один
+// переданный ему валидатор вернул true
+func or(funcs ...validator) validator {
+	return func(s string) bool {
+		for _, val := range funcs {
+			if val(s) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// password содержит строку со значением пароля и валидатор
+type password struct {
+	value string
+	validator
+}
+
+// isValid() проверяет, что пароль корректный, согласно
+// заданному для пароля валидатору
+func (p *password) isValid() bool {
+	return p.validator(p.value)
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+func main() {
+	var s string
+	fmt.Scan(&s)
+	// валидатор, который проверяет, что пароль содержит буквы и цифры,
+	// либо его длина не менее 10 символов
+	validator := or(and(digits, letters), or(minlen(10)))
+	p := password{s, validator}
+	fmt.Println(p.isValid())
+}
